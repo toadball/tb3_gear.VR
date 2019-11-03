bb16e1dbb159feb6894e29ccd56ed981a9d03854
Andrew Somerville <asmsandrew@googlemail.com>
2019-11-03 02:08:58 +0000

    Updated examples

--- tb3/loadouts.hpp
+++ tb3/loadouts.hpp
@@ -68,14 +68,19 @@ class TB3_Gear {//Gear definitions stay within this.
 			backpackRandom = 1;
 			vestRandom = 1;
 		};
-
+		class RandomisedUnit2: RandomisedUnit {
+			weapons[] = {arifle_TRG21_GL_F,Rangefinder};
+			priKit[] = {optic_Holosight_khk_F,acc_pointer_IR,30Rnd_556x45_Stanag,1Rnd_HE_Grenade_shell};
+			vestRandom = 0;
+			vest[] = {V_CarrierRigKBT_01_light_Olive_F};
+		};
 		class ExampleVehicle {
-			vehCargoWeapons[] = {{"launch_NLAW_F",8}};
+			vehCargoWeapons[] = {{"launch_NLAW_F",2}};
 			vehCargoMagazines[] = {
 				{"30Rnd_556x45_Stanag",20},
 				{"30Rnd_556x45_Stanag_Tracer_Yellow",20},
-				{"200Rnd_65x39_cased_Box",4},
-				{"200Rnd_65x39_cased_Box_Tracer",4},
+        {"200Rnd_556x45_box_red_f",4},
+        {"200Rnd_556x45_box_tracer_red_f",4},
 				{"HandGrenade",12},
 				{"SmokeShell",12},
 				{"1Rnd_HE_Grenade_shell",20}
@@ -87,47 +92,32 @@ class TB3_Gear {//Gear definitions stay within this.
 				{"ToolKit",1}
 			};
 			vehCargoRucks[] = {
-				{"B_Kitbag_sgg",2,{
-					{"30Rnd_556x45_Stanag",6},
-					{"30Rnd_556x45_Stanag_Tracer_Red",2},
-					{"HandGrenade",2},
-					{"SmokeShell",2},
-					{"SmokeShellRed",2},
-					{"SmokeShellGreen",2},
-					{"I_IR_Grenade",1},
-					{"1Rnd_SmokeRed_Grenade_shell",2},
-					{"1Rnd_HE_Grenade_shell",6},
-					{"Chemlight_green",6},
-					{"FirstAidKit",2}
-				}},
-				{"B_Kitbag_sgg",3,{
-					{"30Rnd_65x39_caseless_green",4},
-					{"30Rnd_65x39_caseless_green_mag_Tracer",2},
-					{"HandGrenade",1},
-					{"SmokeShell",1},
-					{"O_IR_Grenade",1},
-					{"Chemlight_Red",6},
-					{"FirstAidKit",2}
-				}}
+        {"B_Kitbag_rgr",2,{
+           {"30Rnd_556x45_Stanag_red",6},
+           {"30Rnd_556x45_Stanag_Tracer_Red",2},
+           {"HandGrenade",2},
+           {"SmokeShell",2},
+           {"Chemlight_green",2},
+           {"B_IR_Grenade",1},
+           {"200Rnd_556x45_box_red_f",2},
+           {"200Rnd_556x45_box_tracer_red_f",2}
+          }
+        }
 			};
 		};
 		class ExampleVehicle2: ExampleVehicle {
-			vehCargoRucks[] = {
-				{"B_AssaultPack_ocamo",1,{
-					{"hgun_Pistol_heavy_01_F",2},
-					{"11Rnd_45ACP_Mag",8}
-				}},
-				{"B_AssaultPack_ocamo",1,{
-					{"ACRE_PRC77",1}
-				}},
-				{"B_AssaultPack_ocamo",1,{
-					{"ACE_fieldDressing",2},
-					{"ACE_morphine",1},
-					{"ACE_tourniquet",1},
-					{"ACE_epinephrine",1},
-					{"ACE_packingBandage",2},
-					{"ACE_quikclot",2}
-				}}
+      vehCargoRucks[] = {
+        {"B_Kitbag_rgr",2,{
+           {"30Rnd_556x45_Stanag_red",6},
+           {"30Rnd_556x45_Stanag_Tracer_Red",2},
+           {"HandGrenade",2},
+           {"SmokeShell",2},
+           {"Chemlight_green",2},
+           {"B_IR_Grenade",1},
+           {"200Rnd_556x45_box_red_f",2},
+           {"200Rnd_556x45_box_tracer_red_f",2}
+          }
+        }
 			};
 		};
 	};
